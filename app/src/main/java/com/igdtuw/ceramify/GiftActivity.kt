ef74dea380453cf0fa64cb4d9707d8facef4f946
Anish-Upadhyay <anishupadhyay006@gmail.com>
2025-04-22 14:23:07 +0530

    Buy pages are added

--- app/src/main/java/com/igdtuw/ceramify/GiftActivity.kt
+++ app/src/main/java/com/igdtuw/ceramify/GiftActivity.kt
@@ -1,31 +1,24 @@
 package com.igdtuw.ceramify
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.igdtuw.buypage.databinding.ActivityGiftBinding
-import com.igdtuw.ceramify.databinding.ActivityBuyMainBinding
+import com.igdtuw.ceramify.databinding.ActivityGiftBinding
 
 class GiftActivity : AppCompatActivity() {
 
-    private lateinit var addtocartbtn: Button
-    private lateinit var secondcartbtn: Button
-    private lateinit var binding:ActivityBuyMainBinding
+    private lateinit var binding: ActivityGiftBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = ActivityBuyMainBinding.inflate(layoutInflater)
+
+        // Inflate layout using ViewBinding
+        binding = ActivityGiftBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        addtocartbtn = findViewById(R.id.addToCartBtn1)
-        secondcartbtn = findViewById(R.id.addToCartBtn2)
-
+        // Set onClickListeners
         binding.addToCartBtn1.setOnClickListener {
             Toast.makeText(this, "Added in cart!!", Toast.LENGTH_SHORT).show()
         }
@@ -33,8 +26,5 @@ class GiftActivity : AppCompatActivity() {
         binding.addToCartBtn2.setOnClickListener {
             Toast.makeText(this, "Added in cart!!", Toast.LENGTH_SHORT).show()
         }
-
-
     }
-
 }
