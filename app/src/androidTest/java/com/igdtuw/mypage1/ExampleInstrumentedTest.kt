0fa323727905053dbd071217a06f1ff3982ada45
ankitayadv <ankita.yadv2209@gmail.com>
2025-04-24 13:52:27 +0530

    Update ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/igdtuw/mypage1/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/igdtuw/mypage1/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.igdtuw.mypage1
+package com.igdtuw.ceramify
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.igdtuw.mypage1", appContext.packageName)
+        assertEquals("com.igdtuw.ceramify", appContext.packageName)
     }
 }
